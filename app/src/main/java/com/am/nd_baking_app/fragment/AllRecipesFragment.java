c6f297e7da776eb4983fbcf4aa7d5d89bce6c072
Abed.Murad <abdallah.murad@mail.com>
2018-11-14 03:58:44 +0100

    feat: Add ButterKnife Dependency

--- app/src/main/java/com/am/nd_baking_app/fragment/AllRecipesFragment.java
+++ app/src/main/java/com/am/nd_baking_app/fragment/AllRecipesFragment.java
@@ -1,6 +1,13 @@
 package com.am.nd_baking_app.fragment;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
+
+import com.am.nd_baking_app.R;
 
 public class AllRecipesFragment extends Fragment {
+
+
 }
