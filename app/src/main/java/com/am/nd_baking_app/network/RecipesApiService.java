77ad28be406a7f0ab05993162dbaa28c28c16d31
Abed.Murad <abdallah.murad@mail.com>
2018-11-14 04:21:13 +0100

    Style: Change RecipesApiService to inteface

--- app/src/main/java/com/am/nd_baking_app/network/RecipesApiService.java
+++ app/src/main/java/com/am/nd_baking_app/network/RecipesApiService.java
@@ -1,4 +1,14 @@
 package com.am.nd_baking_app.network;
 
-public class RecipesApiService {
+import com.am.nd_baking_app.model.Recipe;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+interface RecipesApiService {
+    @GET("topher/2017/May/59121517_baking/baking.json")
+    Call<List<Recipe>> getRecipes();
+
 }
