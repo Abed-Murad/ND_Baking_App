28870be87ed56a440b23fbaabd36fa59adfc702b
Abed.Murad <abdallah.murad@mail.com>
2018-11-14 04:44:46 +0100

    feat: implementing onCreateView()

--- app/src/main/java/com/am/nd_baking_app/fragment/AllRecipesFragment.java
+++ app/src/main/java/com/am/nd_baking_app/fragment/AllRecipesFragment.java
@@ -3,18 +3,30 @@ package com.am.nd_baking_app.fragment;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.am.nd_baking_app.R;
+import com.am.nd_baking_app.adapter.RecipesAdapter;
 import com.am.nd_baking_app.model.Recipe;
+import com.am.nd_baking_app.network.RecipesApiCallback;
+import com.am.nd_baking_app.network.RecipesApiManager;
+import com.am.nd_baking_app.util.FUNC;
 import com.am.nd_baking_app.util.MyApplication;
+import com.am.nd_baking_app.util.SpacingItemDecoration;
 
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 public class AllRecipesFragment extends Fragment {
@@ -25,19 +37,129 @@ public class AllRecipesFragment extends Fragment {
     @BindView(R.id.noDataContainer)
     ConstraintLayout mNoDataContainer;
 
+    private static String RECIPES_KEY = "recipes";
+
+
     private OnRecipeClickListener mListener;
     private Unbinder unbinder;
     private List<Recipe> mRecipes;
     private MyApplication myMyApplication;
 
 
-
-
-
     public AllRecipesFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment bind view to butter knife
+        View viewRoot = inflater.inflate(R.layout.fragment_all_recipes, container, false);
+        unbinder = ButterKnife.bind(this, viewRoot);
+
+        mPullToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loadRecipes();
+            }
+        });
+
+        mNoDataContainer.setVisibility(View.VISIBLE);
+        setupRecyclerView();
+
+        // Get the IdlingResource instance
+        myMyApplication = (MyApplication) getActivity().getApplicationContext();
+
+        myMyApplication.setIdleState(false);
+
+
+        if (savedInstanceState != null && savedInstanceState.containsKey(RECIPES_KEY)) {
+            mRecipes = savedInstanceState.getParcelableArrayList(RECIPES_KEY);
+
+            mRecipesRecyclerView.setAdapter(new RecipesAdapter(getActivity().getApplicationContext(), mRecipes, new RecipesAdapter.OnItemClickListener() {
+                @Override
+                public void onItemClick(int position) {
+                    mListener.onRecipeSelected(mRecipes.get(position));
+                }
+            }));
+            dataLoadedTakeCareLayout();
+        }
+        return viewRoot;
+    }
+
+
+    private void setupRecyclerView() {
+        mRecipesRecyclerView.setVisibility(View.GONE);
+        mRecipesRecyclerView.setHasFixedSize(true);
+
+        boolean twoPaneMode = getResources().getBoolean(R.bool.twoPaneMode);
+        if (twoPaneMode) {
+            mRecipesRecyclerView.setLayoutManager(new GridLayoutManager(getActivity().getApplicationContext(), 3));
+        } else {
+            mRecipesRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext(), LinearLayoutManager.VERTICAL, false));
+        }
+
+        mRecipesRecyclerView.addItemDecoration(new SpacingItemDecoration((int) getResources().getDimension(R.dimen.margin_medium)));
+        mRecipesRecyclerView.addOnItemTouchListener(new RecyclerView.SimpleOnItemTouchListener());
+    }
+
+
+    private void loadRecipes() {
+        // Set SwipeRefreshLayout that refreshing in case that loadRecipes get called by the networkChangeReceiver
+        if (FUNC.isNetworkAvailable(getActivity().getApplicationContext())) {
+            mPullToRefresh.setRefreshing(true);
+
+            RecipesApiManager.getInstance().getRecipes(new RecipesApiCallback<List<Recipe>>() {
+                @Override
+                public void onResponse(final List<Recipe> result) {
+                    if (result != null) {
+                        mRecipes = result;
+                        mRecipesRecyclerView.setAdapter(new RecipesAdapter(getActivity().getApplicationContext(), mRecipes, new RecipesAdapter.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(int position) {
+                                mListener.onRecipeSelected(mRecipes.get(position));
+                            }
+                        }));
+                        // Set the default recipe for the widget
+/*
+                        if (Prefs.loadRecipe(getActivity().getApplicationContext()) == null) {
+                            AppWidgetService.updateWidget(getActivity(), mRecipes.get(0));
+                        }
+*/
+
+                    } else {
+
+                        FUNC.makeSnackBar(getActivity(), getView(), getString(R.string.failed_to_load_data), true);
+
+                    }
+                    dataLoadedTakeCareLayout();
+
+                }
+
+                @Override
+                public void onCancel() {
+                    dataLoadedTakeCareLayout();
+                }
+
+            });
+        } else {
+
+            FUNC.makeSnackBar(getActivity(), getView(), getString(R.string.no_internet), true);
+        }
+    }
+    /**
+     * Check if data is loaded and show/hide Recipes RecyclerView & NoDataContainer regarding the recipes data state
+     */
+    private void dataLoadedTakeCareLayout() {
+        boolean loaded = mRecipes != null && mRecipes.size() > 0;
+        mPullToRefresh.setRefreshing(false);
+
+        mRecipesRecyclerView.setVisibility(loaded ? View.VISIBLE : View.GONE);
+        mNoDataContainer.setVisibility(loaded ? View.GONE : View.VISIBLE);
+
+        myMyApplication.setIdleState(true);
+
+    }
 
     public interface OnRecipeClickListener {
         void onRecipeSelected(Recipe recipe);
