714ae735817f017b8cbf4a7abc232925c6d10c33
Abed.Murad <abdallah.murad@mail.com>
2018-11-14 04:03:31 +0100

    feat: Add Jackson Dependency

--- app/src/main/java/com/am/nd_baking_app/fragment/AllRecipesFragment.java
+++ app/src/main/java/com/am/nd_baking_app/fragment/AllRecipesFragment.java
@@ -1,5 +1,8 @@
 package com.am.nd_baking_app.fragment;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -7,7 +10,19 @@ import android.support.v7.widget.RecyclerView;
 
 import com.am.nd_baking_app.R;
 
-public class AllRecipesFragment extends Fragment {
+import butterknife.BindView;
+import butterknife.Unbinder;
 
+public class AllRecipesFragment extends Fragment {
+    @BindView(R.id.recipes_recycler_view)
+    RecyclerView mRecipesRecyclerView;
+    @BindView(R.id.pull_to_refresh)
+    SwipeRefreshLayout mPullToRefresh;
+    @BindView(R.id.noDataContainer)
+    ConstraintLayout mNoDataContainer;
+
+    public AllRecipesFragment() {
+        // Required empty public constructor
+    }
 
 }
