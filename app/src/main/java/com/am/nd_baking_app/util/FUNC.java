f65d75399aa3989688d6c5f6dd42a8d1b98108aa
Abed.Murad <abdallah.murad@mail.com>
2018-11-14 04:25:33 +0100

    Res: Add Error and Info Colors

--- app/src/main/java/com/am/nd_baking_app/util/FUNC.java
+++ app/src/main/java/com/am/nd_baking_app/util/FUNC.java
@@ -2,6 +2,12 @@ package com.am.nd_baking_app.util;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.TextView;
+
+import com.am.nd_baking_app.R;
 
 public class FUNC {
     public static boolean isNetworkAvailable(Context context) {
@@ -9,4 +15,13 @@ public class FUNC {
         return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
     }
 
+    public static void makeSnackBar(Context context, View view, String text, boolean error) {
+        final Snackbar snackbar;
+        snackbar = Snackbar.make(view, text, Snackbar.LENGTH_LONG);
+        View snackBarView = snackbar.getView();
+        snackBarView.setBackgroundColor(ContextCompat.getColor(context, error ? R.color.colorError : R.color.colorInfo));
+        TextView textView = snackBarView.findViewById(android.support.design.R.id.snackbar_text); //Get reference of snackbar textview
+        textView.setMaxLines(3);
+        snackbar.show();
+    }
 }
