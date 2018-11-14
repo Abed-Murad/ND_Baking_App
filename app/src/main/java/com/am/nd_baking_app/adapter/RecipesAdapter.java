f6b18a267a8b4546a5b111b9358403925b6df0c8
Abed.Murad <abdallah.murad@mail.com>
2018-11-14 04:33:54 +0100

    feat: finish RecipesAdapter & Add GildeModule

--- app/src/main/java/com/am/nd_baking_app/adapter/RecipesAdapter.java
+++ app/src/main/java/com/am/nd_baking_app/adapter/RecipesAdapter.java
@@ -1,4 +1,92 @@
 package com.am.nd_baking_app.adapter;
 
-public class RecipesAdapter  {
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.AppCompatImageView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.am.nd_baking_app.R;
+import com.am.nd_baking_app.model.Recipe;
+import com.am.nd_baking_app.util.GlideApp;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.RecipeViewHolder> {
+    private Context mContext;
+    private List<Recipe> mRecipes;
+    private OnItemClickListener mOnItemClickListener;
+
+    public RecipesAdapter(Context context, List<Recipe> recipes, OnItemClickListener onItemClickListener) {
+        this.mContext = context;
+        this.mRecipes = recipes;
+        this.mOnItemClickListener = onItemClickListener;
+    }
+
+    @NonNull
+    @Override
+    public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recipe_list_item, parent, false);
+
+        return new RecipeViewHolder(view);
+    }
+
+    @SuppressLint("RecyclerView")
+    @Override
+    public void onBindViewHolder(@NonNull RecipeViewHolder holder, final int position) {
+        holder.mTvRecipeName.setText(mRecipes.get(position).getName());
+        holder.mTvServings.setText(mContext.getString(R.string.servings, mRecipes.get(position).getServings()));
+
+        String recipeImage = mRecipes.get(position).getImage();
+        if (!recipeImage.isEmpty()) {
+            GlideApp.with(mContext)
+                    .load(recipeImage)
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .placeholder(R.drawable.recipe_placeholder)
+                    .into(holder.mIvRecipe);
+        }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mOnItemClickListener != null)
+                    mOnItemClickListener.onItemClick(position);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRecipes.size();
+    }
+
+    public class RecipeViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.recipe_name_text)
+        TextView mTvRecipeName;
+        @BindView(R.id.servings_text)
+        TextView mTvServings;
+        @BindView(R.id.recipe_image)
+        AppCompatImageView mIvRecipe;
+
+        RecipeViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+    }
+
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+    }
+
 }
---
+++ app/src/main/java/com/am/nd_baking_app/util/GlideAppModule.java
@@ -0,0 +1,7 @@
+package com.am.nd_baking_app.util;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+@GlideModule
+public class GlideAppModule extends AppGlideModule {
+}
