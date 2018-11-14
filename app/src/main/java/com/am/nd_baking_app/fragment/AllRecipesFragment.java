49781eaaa03c5b9b1765b44305d9637dcf9d22a8
Abed.Murad <abdallah.murad@mail.com>
2018-11-14 04:13:43 +0100

    files: Create My Application

--- app/src/main/java/com/am/nd_baking_app/fragment/AllRecipesFragment.java
+++ app/src/main/java/com/am/nd_baking_app/fragment/AllRecipesFragment.java
@@ -11,6 +11,8 @@ import android.support.v7.widget.RecyclerView;
 import com.am.nd_baking_app.R;
 import com.am.nd_baking_app.model.Recipe;
 
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.Unbinder;
 
@@ -21,6 +23,11 @@ public class AllRecipesFragment extends Fragment {
     SwipeRefreshLayout mPullToRefresh;
     @BindView(R.id.noDataContainer)
     ConstraintLayout mNoDataContainer;
+
+    private OnRecipeClickListener mListener;
+    private Unbinder unbinder;
+    private List<Recipe> mRecipes;
+
 
     public AllRecipesFragment() {
         // Required empty public constructor
---
+++ app/src/main/java/com/am/nd_baking_app/util/MyApplication.java
@@ -0,0 +1,7 @@
+package com.am.nd_baking_app.util;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+    
+}
