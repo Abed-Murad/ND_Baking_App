b4f7b6394ac2b6e7ea38d4e7a293f782ce36f6cc
Abed.Murad <abdallah.murad@mail.com>
2018-11-14 04:17:03 +0100

    files: Create RecipesIdlingResource

--- app/src/main/java/com/am/nd_baking_app/util/MyApplication.java
+++ app/src/main/java/com/am/nd_baking_app/util/MyApplication.java
@@ -1,7 +1,57 @@
 package com.am.nd_baking_app.util;
 
 import android.app.Application;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
+import android.support.test.espresso.IdlingResource;
+
+import com.am.nd_baking_app.BuildConfig;
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
 
 public class MyApplication extends Application {
-    
+    // The Idling Resource which will be null in production.
+    @Nullable
+    private RecipesIdlingResource mIdlingResource;
+
+    /**
+     * Only called from test, creates and returns a new {@link RecipesIdlingResource}.
+     */
+    @VisibleForTesting
+    @NonNull
+    private IdlingResource initializeIdlingResource() {
+        if (mIdlingResource == null) {
+            mIdlingResource = new RecipesIdlingResource();
+        }
+        return mIdlingResource;
+    }
+
+    public MyApplication() {
+
+        // The IdlingResource will be null in production.
+        if (BuildConfig.DEBUG) {
+            initializeIdlingResource();
+        }
+
+        Logger.addLogAdapter(new AndroidLogAdapter() {
+            @Override
+            public boolean isLoggable(int priority, String tag) {
+                // Enable logging only on debug
+                return BuildConfig.DEBUG;
+            }
+        });
+
+    }
+
+    public void setIdleState(boolean state) {
+        if (mIdlingResource != null)
+            mIdlingResource.setIdleState(state);
+    }
+
+    @Nullable
+    public RecipesIdlingResource getIdlingResource() {
+        return mIdlingResource;
+    }
+
 }
