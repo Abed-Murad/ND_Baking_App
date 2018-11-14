938fb3e068a6ac77898314a04235b7b9d1f713cb
Abed.Murad <abdallah.murad@mail.com>
2018-11-14 03:33:37 +0100

    file: move MainActivity.java to activity pkg

--- app/src/main/java/com/am/nd_baking_app/activity/MainActivity.java
+++ app/src/main/java/com/am/nd_baking_app/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.am.nd_baking_app;
+package com.am.nd_baking_app.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.am.nd_baking_app.R;
 
 public class MainActivity extends AppCompatActivity {
 
