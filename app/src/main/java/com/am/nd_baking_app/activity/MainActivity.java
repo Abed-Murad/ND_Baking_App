0d9b1667865e07c4208cdefafe42544ebdc8028c
Abed.Murad <abdallah.murad@mail.com>
2018-11-15 01:15:35 +0100

    files: Create RecipeInfoActivity

--- app/src/main/java/com/am/nd_baking_app/activity/MainActivity.java
+++ app/src/main/java/com/am/nd_baking_app/activity/MainActivity.java
@@ -1,15 +1,26 @@
 package com.am.nd_baking_app.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.am.nd_baking_app.R;
+import com.am.nd_baking_app.fragment.AllRecipesFragment;
+import com.am.nd_baking_app.model.Recipe;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AllRecipesFragment.OnRecipeClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onRecipeSelected(Recipe recipe) {
+        Intent intent = new Intent(this, RecipeInfoActivity.class);
+        intent.putExtra(RecipeInfoActivity.RECIPE_KEY, recipe);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/am/nd_baking_app/activity/RecipeInfoActivity.java
@@ -0,0 +1,5 @@
+package com.am.nd_baking_app.activity;
+
+class RecipeInfoActivity {
+    public static final String RECIPE_KEY = "recipe_k";
+}
